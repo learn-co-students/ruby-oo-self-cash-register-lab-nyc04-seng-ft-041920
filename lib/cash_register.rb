fd8390eafd689b040b80eaa1ae575ae90d6ef449
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-05-16 10:26:15 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,23 @@
+#5/14/2020 - 5/15/2020 7:00am - 8:39pm
+
 require 'pry'
 class CashRegister
 
     #@@all = [] #global scope
-    attr_accessor :total, :discount, :items, :price, :last_price #obj attribute(getter method + setter method)
+    attr_accessor :total, :discount, :items, :last_transaction #obj attribute(getter method + setter method)
    # attr_reader :last_price 
 
+
+
+#    CashRegister.new()  # discount here will initialize to 20 (default)
+#    CashRegister.new(55). #discount here will initialize to 55 
+
+
+
     def initialize(discount = 0) #obj initialization
-        @total = 0 #instance variables
-        @discount = discount #instance variables
-        @items = []
-        @last_price = 0
+        @total = 0 #instance variable
+        @discount = discount #instance variable
+        @items = [] #instance variable
 
     end
 
@@ -28,7 +36,6 @@ class CashRegister
     # end
 
     def add_item(item, price, quant=1)
-        self.price = price
         self.total += price * quant
 
         quant.times { |a| self.items << item }
@@ -44,21 +51,135 @@ class CashRegister
         # end
         
         
-        self.last_price = price * quant  
+        self.last_transaction = price * quant  
     end
     #binding.pry
 
     def apply_discount 
+        #binding.pry
         if self.discount == 0
             "There is no discount to apply."
         else
-            self.total -= 200
+            self.total = (total * ((100.0 - self.discount.to_f)/100)).to_i
          "After the discount, the total comes to $#{self.total}."
          end
     end
 
     def void_last_transaction
-        self.total -= self.last_price
+        self.total -= self.last_transaction
     end
 
 end
+
+
+
+
+
+# 5/15/2020 Fri 8:40pm - 9:11 (31mins)
+
+# class CashRegister
+
+#     attr_accessor :total, :discount, :items, :last_transaction
+
+#     def initialize(discount = 0)
+#         @total = 0
+#         @discount = discount
+#         @items = []
+#         @last_transaction = last_transaction
+#     end
+
+#     def add_item (title, price, quant = 1)
+       
+#         self.total += price * quant
+
+#         quant.times {|i| self.items << title }
+
+#         @last_transaction = price * quant
+#     end
+
+#     def apply_discount
+#         if self.discount == 0 
+#             "There is no discount to apply."
+#         else
+#             self.total -= 200
+#             "After the discount, the total comes to $#{self.total}."
+#         end  
+#     end
+
+#     def void_last_transaction
+#         self.total -= self.last_transaction
+#     end
+    
+# end
+
+# # 5/15 9:30 - 9:53 (23mins)
+# class CashRegister
+
+#     attr_accessor :total, :discount, :last_transaction, :items #obj attribute(getter method + setter method)
+
+#     def initialize(discount = 0) ## why does this work??
+#         @total = 0 #instace variable
+#         @discount = discount #instance variable
+#         @items = []
+#         @last_transaction = 0
+#     end
+
+#     def add_item(title, price, quant = 1)
+   
+#         self.total += price * quant
+        
+#         quant.times { |i| self.items << title }
+
+#         self.last_transaction = price * quant
+#     end
+
+#     def apply_discount 
+#         if self.discount == 0
+#             "There is no discount to apply."
+#         else
+#             self.total -= 200
+#             "After the discount, the total comes to $#{self.total}."
+#         end
+#     end
+
+#     def void_last_transaction
+#         self.total -= self.last_transaction
+#     end
+
+# end
+
+
+# #5/13/2020 9:59 - 10:09 (10mins!)
+
+# class CashRegister
+
+#     attr_accessor :total, :discount, :last_transaction, :items
+
+#     def initialize (discount = 0)
+#         @total = 0
+#         @discount = discount
+#         @last_transaction = 0
+#         @items = []
+#     end
+
+#     def add_item(title, price, quant = 1)
+#         self.total += price * quant
+
+#         quant.times {|i| @items << title}
+
+#         @last_transaction = price * quant
+#     end
+
+#     def apply_discount
+#         if self.discount == 0 
+#             "There is no discount to apply."
+#         else
+#             self.total -= 200
+#             "After the discount, the total comes to $#{self.total}."
+#         end
+#     end
+
+#     def void_last_transaction
+#         @total -= @last_transaction
+#     end
+# end
